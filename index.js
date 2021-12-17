dea3d4769414486fbeeac2e9674829ea40eba965
brenerpittner <pittner@live.com>
2021-12-17 13:24:12 -0300

    Atualizado index.js

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 var today = new Date();
-//console.log(today);
-var year = today.getFullYear();
-var month = today.getMonth()+1;
-var day = today.getDate();
-var date_actual =day+"/"+month+"/"+year;
+var date_actual = today.getDate() + "/" + (today.getMonth()+1) + "/" +today.getFullYear();
 //console.log(date_actual);
 
-
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let participantes = [`Brener`, `João`, `Marcelo`]
+let participantes = ['Brener','João','Fabiana','Marcelo','Bruna','Guilherme','Vitor','Luiz','Amanda', 'José']
+let palestrantes = ['Gabriel', 'Vitor', 'Henrique']
 
-rl.question("Informe sua idade: ", function(idade) {
-    if (idade >= 18){
-        rl.question("Informe a data do evento: ", function(data) {
-            if (data > date_actual){
-                console.log(`Evento cadastrado`);
-                rl.close();
-            }else{
-                console.log(`Cadastro não permitido pela data`);
-                rl.close();                    
-                };
-            });
-    }else{
-        console.log(`Cadastro não permitido pela idade`);
-        rl.close();
-    };
-});
+if (participantes.length < 10){
+    rl.question('Informe sua idade: ', function(idade) {
+        if (idade >= 18){
+            rl.question('Informe a data do evento: ', function(data) {
+                if (data > date_actual){
+                    participantes.push('José')  
+                    console.log('Evento cadastrado');
+                    rl.close();
+                }else{
+                    console.log('Cadastro não permitido pela data');
+                    rl.close();                    
+                    };
+                });
+        }else{
+            console.log('Cadastro não permitido pela idade');
+            rl.close();
+        };
+    });
+}else{
+    console.log('Cadastro não permitido por excedido o limite');
+    rl.close();  
+};
 
-
-
-rl.on("close", function() {
-    console.log("\nFim");
+rl.on('close', function() {
+    console.log(participantes);
+    console.log(palestrantes);
+    console.log('\nFim');
     process.exit(0);
 });
